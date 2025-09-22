2317c0cec5381e5408ad75e00f2246b2050cc86f
Irineu Marcos <spetoki@gmail.com>
2025-09-22 10:27:51 +0000

    o app bugou, desbugue ele

--- src/app/(tabs)/layout.tsx
+++ src/app/(tabs)/layout.tsx
@@ -3,14 +3,17 @@
 
 import type { ReactNode } from 'react';
 import BottomNav from '@/components/bottom-nav';
+import { StockProvider } from '@/hooks/use-stock';
 
 export default function TabsLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-1 pb-24 pt-4">
-        {children}
-      </main>
-      <BottomNav />
-    </div>
+    <StockProvider>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-1 pb-24 pt-4">
+          {children}
+        </main>
+        <BottomNav />
+      </div>
+    </StockProvider>
   );
 }
